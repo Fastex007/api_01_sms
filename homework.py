aedef48169e63f630dc391340934bffd10305703
Fastex007 <fastex007@yandex.ru>
2021-02-02 17:31:17 +0300

    First commit. SMS sending

--- homework.py
+++ homework.py
@@ -1,21 +1,44 @@
+import os
+import requests
 import time
 
-import requests
+from dotenv import load_dotenv
+
 from twilio.rest import Client
 
-# здесь проинициализируйте Client 
+
+load_dotenv()
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(account_sid, auth_token)
+
 
 def get_status(user_id):
+    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
+    vk_api_v = os.getenv('VK_API_V')
+    vk_base_url = os.getenv('VK_BASE_URL')
+
     params = {
-        ...
+        'user_ids': user_id,
+        'v': vk_api_v,
+        'access_token': vk_access_token,
+        'fields': 'online'
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    user_state = requests.post(vk_base_url, params=params)
+    user_state = user_state.json().get('response')[0]
+    return user_state.get('online')
 
 
 def send_sms(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    number_from = os.getenv('NUMBER_FROM')
+    number_to = os.getenv('NUMBER_TO')
+    message = client.messages.create(
+        body=sms_text,
+        from_=number_from,
+        to=number_to
+    )
+    return message.sid
 
 
 if __name__ == '__main__':
